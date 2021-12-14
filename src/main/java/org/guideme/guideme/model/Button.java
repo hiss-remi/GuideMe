384c4ea4b4671fe787d6bdc6aed9e8c4901844e6
Remi Hiyama <hiss-remi@users.noreply.github.com>
2021-12-14 07:57:18 -0600

    Continued work on HashCommandProcessor Added hash parameter support to Button (needs testing)

--- src/main/java/org/guideme/guideme/model/Button.java
+++ src/main/java/org/guideme/guideme/model/Button.java
@@ -2,8 +2,14 @@ package org.guideme.guideme.model;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import org.eclipse.swt.graphics.Color;
 import org.guideme.guideme.settings.ComonFunctions;
+import org.guideme.guideme.util.HashCommandProcessor;
+import org.guideme.guideme.util.HashParam;
+import org.guideme.guideme.util.HashParam.*;
+import org.mozilla.javascript.NativeObject;
 
 public class Button  implements Comparable<Button>
 {
@@ -96,7 +102,94 @@ public class Button  implements Comparable<Button>
 		this.setDefaultBtn(defaultBtn);
 	}
 
-	
+	public Button(NativeObject params) {
+		HashCommandProcessor processor = new HashCommandProcessor(mapper);
+		processor.parse(params);
+
+		this.target = processor.getString("target");
+		this.text = processor.getString("text");
+		this.ifNotSet = processor.getString("ifnotset");
+		this.ifSet = processor.getString("ifset");
+		this.set = processor.getString("set");
+		this.unSet = processor.getString("unset");
+		String temp = processor.getString("jscript");
+		if (temp == "")
+		{
+			temp = processor.getString("script");
+			if (temp == "")
+				temp = processor.getString("javascript");
+		}
+		this.jScript = temp;
+		this.image = processor.getString("image");
+		this.hotKey = processor.getString("hotkey");
+		this.fontName = processor.getString("fontname");
+		this.fontHeight = processor.getString("fontheight");
+		this.sortOrder = processor.getInt("sortorder");
+
+		Color temp2 = processor.getColor("bgcolor1", false);
+		if (temp2 == null)
+			temp2 = comonFunctions.getColor("white");
+		this.bgColor1 = temp2;
+
+		temp2 = processor.getColor("bgcolor2", false);
+		if (temp2 == null)
+			temp2 = this.bgColor1;
+		this.bgColor2 = temp2;
+
+		temp2 = processor.getColor("fontcolor", false);
+		if (temp2 == null)
+			temp2 = comonFunctions.getColor("black");
+		this.fontColor = temp2;
+
+		temp = processor.getString("ifbefore");
+		if (temp == "")
+			this.ifBefore = null;
+		else
+			this.ifBefore = LocalTime.parse(temp);
+
+		temp = processor.getString("ifafter");
+		if (temp == "")
+			this.ifAfter = null;
+		else
+			this.ifAfter = LocalTime.parse(temp);
+
+		this.disabled = processor.getBool("disabled");
+		this.id = processor.getString("id");
+		this.scriptVar = processor.getString("scriptvar");
+		this.setDefaultBtn(processor.getBool("default") || processor.getBool("defaultbtn") || processor.getBool("defaultbutton"));
+	}
+
+	protected static HashMap<String, HashParam> mapper = createMapper();
+
+	protected static HashMap<String, HashParam> createMapper() {
+		HashMap<String, HashParam> temp = new HashMap<String, HashParam>();
+		temp.put("target", new HashParam(""));
+		temp.put("text", new HashParam(""));
+		temp.put("ifnotset", new HashParam(""));
+		temp.put("ifset", new HashParam(""));
+		temp.put("set", new HashParam(""));
+		temp.put("unset", new HashParam(""));
+		temp.put("javascript", new HashParam(""));
+		temp.put("jscript", new HashParam(""));
+		temp.put("script", new HashParam(""));
+		temp.put("image", new HashParam(""));
+		temp.put("hotkey", new HashParam(""));
+		temp.put("fontname", new HashParam(""));
+		temp.put("fontheight", new HashParam(""));
+		temp.put("sortorder", new HashParam(Type.INTEGER, 1));
+		temp.put("bgcolor1", new HashParam(Type.COLOR, null));
+		temp.put("bgcolor2", new HashParam(Type.COLOR, null));
+		temp.put("fontcolor", new HashParam(Type.COLOR, null));
+		temp.put("ifbefore", new HashParam(""));
+		temp.put("ifafter", new HashParam(""));
+		temp.put("disabled", new HashParam(Type.BOOLEAN, false));
+		temp.put("id", new HashParam(""));
+		temp.put("scriptvar", new HashParam(""));
+		temp.put("default", new HashParam(Type.BOOLEAN, false));
+		temp.put("defaultbtn", new HashParam(Type.BOOLEAN, false));
+		temp.put("defaultbutton", new HashParam(Type.BOOLEAN, false));
+		return temp;
+	}
 	
 	public void setUnSet(ArrayList<String> setList)
 	{
